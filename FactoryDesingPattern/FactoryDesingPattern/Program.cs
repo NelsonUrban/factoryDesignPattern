a8d921befd5f3b70f6334b1ae2ed4638123b938c
Nelson Nehemias Urban Marten <nurban@mardom.com>
2022-03-11 08:17:29 -0400

    initial commit

---
+++ FactoryDesingPattern/FactoryDesingPattern/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FactoryDesingPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MemberShipFactory obj = new ConcreteFactory();
+
+            Console.WriteLine("Which membership Charges do you wants ?");
+
+            string type = Console.ReadLine();
+            IGymChages intance = obj.GetGymMembershipChagesType(type);
+
+            Console.WriteLine(intance.GetChages());
+            Console.ReadLine();
+        }
+
+        public interface IGymChages // Product
+        {
+            int GetChages();
+        }
+
+        public class Monthly : IGymChages //Croncrete product
+        {
+            public int GetChages()
+            {
+                return 1000;
+            }
+        }
+
+        public class Yearly : IGymChages //Croncrete product
+        {
+            public int GetChages()
+            {
+                return 4000;
+            }
+        }
+
+        public abstract class MemberShipFactory // Factory  class Creator
+        {
+            public abstract IGymChages GetGymMembershipChagesType(string type);
+        }
+
+        public class ConcreteFactory : MemberShipFactory  // Concrete Creator
+        {
+            public override IGymChages GetGymMembershipChagesType(string type)
+            {
+                switch (type)
+                {
+                    case "monthy":
+                        return new Monthly();
+
+                    case "yearly":
+                        return new Yearly();
+
+                    default:
+                        throw new Exception( $"the type of {type} is not already implemented");
+                }
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# factory Design Pattern
-Learning Factory Design Pattern
+# Factory Desing Pattern
+Repository for Practice  Factory Desing Pattern
+
+### Factory pattern
+- Factory pattern is one of the most used design patterns in Java. This type of design pattern comes under creational pattern as this pattern provides one of the best ways to create an object.
+
+
+### Technology used
+- [x] C#
+- [x] .Net Core 3.1
+- [x] Linq   
+
+### kind of Application
+
+- [x] Console Application   
